4e6f7adc391c6e48a04e23730506ec8c0baeed5b
nirbhi <nirbhipareek@gmail.com>
2021-12-08 12:23:54 +0530

    minor modifictaion in add function

--- math.py
+++ math.py
@@ -1,6 +1,9 @@
+# PAT = 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
 #I need to implement this
 def add(x,y):
-  return x+y
+  if (type(x)==int and type(y)==int):
+  	return x+y
+  return None
 
 # Bob needs to implement this
 def mult(x,y):
